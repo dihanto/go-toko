2ba89b9939d105821446f89afc351eb64d745a3f
dihanto <dihanto2306@gmail.com>
2023-07-10 10:01:46 +0700

    🔥 remove unnecessary file

--- database/migration/20230618110252_create_table_banks.up.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE banks (
-    code SERIAL PRIMARY KEY,
-    name VARCHAR(100),
-    created_at integer,
-    updated_at integer not NULL DEFAULT 0,
-    deleted_at integer
-);
--- model/entity/bank.go
+++
@@ -1,9 +0,0 @@
-package entity
-
-type Bank struct {
-	Code      int
-	Name      string
-	CreatedAt int32
-	UpdatedAt int32
-	DeletedAt int32
-}
